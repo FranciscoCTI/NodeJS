ee4c30400d576cb1fd30f060409dbe858537ea6a
FranciscoCTI <franciscofcch@gmail.com>
2024-08-21 21:22:15 -0400

    #Change

--- mjs/ls-advanced.js
+++ mjs/ls-advanced.js
@@ -5,12 +5,12 @@ const folder = process.argv[2] ?? '.'
 fs.readdir(folder)
     .then(files => {
         files.forEach(file=>{
-        console.log(file)
+        const filePath = path.join(folder, file)
     })
 })
 .catch(err => {
     if(err){
         console.error('error al leer directorio: ' , err)
         return;
-    }
+    }   
 })
