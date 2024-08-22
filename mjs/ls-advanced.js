9f08aade3d9f7f8e380e44d5767638f69c13b43a
FranciscoCTI <franciscofcch@gmail.com>
2024-08-21 21:52:18 -0400

    #Work on ls-advanced.js

--- mjs/ls-advanced.js
+++ mjs/ls-advanced.js
@@ -1,16 +1,42 @@
 const fs = require('node:fs/promises')
+const path = require('node:path')
 
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder)
-    .then(files => {
-        files.forEach(file=>{
+async function ls (folder){
+    let files
+    try
+    {
+        files = await fs.readdir(folder)
+    }
+    catch
+    {
+        console.error('no se pudo leer el directorio ${folder}')
+        process.exit(1)
+    }
+
+    const filesPromises = files.map(async file=>{
         const filePath = path.join(folder, file)
+        let stats
+        try{
+            stats = await fs.stat(filePath)
+        }catch
+        {
+            console.error('no se pudo leer archivo desde ${filepath}')
+            process.exit(1)
+        }
+
+        const isDirectory = stats.isDirectory();
+        const fileType = isDirectory ? 'd' : 'fi'
+        const filesize = stats.size
+        const fileModified = stats.mtime.toLocaleDateString()
+
+        //return '${fileType} ${file} ${filesize.toString()} ${fileModified}';
+        return fileType + ' ' + file + ' ' + filesize.toString() + ' ' + fileModified;
     })
-})
-.catch(err => {
-    if(err){
-        console.error('error al leer directorio: ' , err)
-        return;
-    }   
-})
+
+    const filesInfo = await Promise.all(filesPromises)
+    filesInfo.forEach(fileInfo=> console.log(fileInfo))
+}
+
+ls(folder)
